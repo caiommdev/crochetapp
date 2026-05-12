bb30ece14477a9240af056866c0e40cab2d09a68
caiommdev <caiommarinho@proton.me>
2026-05-12 17:55:56 -0300

    feat(crochetapp): add project

---
+++ src/main/java/org/example/crochetapp/CrochetappApplication.java
@@ -0,0 +1,13 @@
+package org.example.crochetapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrochetappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrochetappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/crochetapp/CrochetappApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.crochetapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrochetappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
