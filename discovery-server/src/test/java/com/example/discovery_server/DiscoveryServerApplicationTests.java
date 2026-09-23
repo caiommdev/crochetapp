fd90c16e4e0186a7e1ef7c8cf4f80233884ce65a
caiommdev <caiommarinho@proton.me>
2026-09-23 10:39:34 -0300

    fix discovery server tests

--- discovery-server/src/test/java/com/example/discovery_server/DiscoveryServerApplicationTests.java
+++ discovery-server/src/test/java/com/example/discovery_server/DiscoveryServerApplicationTests.java
@@ -2,8 +2,9 @@ package com.example.discovery_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.example.discovery_server.DiscoveryServerApplication;
 
-@SpringBootTest
+@SpringBootTest(classes = DiscoveryServerApplication.class)
 class DiscoveryServerApplicationTests {
 
 	@Test
